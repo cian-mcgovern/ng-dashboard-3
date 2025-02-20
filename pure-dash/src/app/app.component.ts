1b568e0fe23ebdbc90c7c5f309620445f9e286b8
cian-mcgovern <cian.mcgovern@tudublin.ie>
2025-02-20 12:32:17 +0000

    working ng app in devcontainer with pure client connection established

--- pure-dash/src/app/app.component.ts
+++ pure-dash/src/app/app.component.ts
@@ -1,17 +1,39 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
+import { PureClient } from 'pure-client'
+import { Observable } from 'rxjs';
 
+interface PureUser {
+	username: string
+}
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'pure-dash';
+  	title = 'pure-dash';
+	users: any = [];
+	constructor() {
+		console.log('hitting here');
+		
+	}
 
-  constructor() {
-    // console.log('hitting here');
+	async ngOnInit() {
+		await this.getPureUsers();
+	  }
+  async getPureUsers() {
+		const url = "";
+		const apiKey = "";
+		const p = new PureClient(url, apiKey);
+		const users = await p.get("users/");
+		console.log("ng onit...");
+		console.log(users);
+		this.users = users;
   }
+  	
 
 }
